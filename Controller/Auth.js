334096a2072fa92ead0ae4e3c61aa0f93a40308c
Jemish sakariya <74782154+jemishsakariya@users.noreply.github.com>
2023-06-12 10:21:30 +0530

    signup auth is created

--- Controller/Auth.js
+++ Controller/Auth.js
@@ -1,7 +1,60 @@
 const User = require("../models/User");
 
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
-exports.signup = async (req, res) => {};
+exports.signup = async (req, res) => {
+  try {
+    const { name, email, password, role } = req.body;
 
-exports.login = async (req, res) => {};
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      return res.status(400).json({
+        success: false,
+        message: "User is already Exists",
+      });
+    }
+
+    // secure password
+    let hashedPassword;
+    try {
+      hashedPassword = await bcrypt.hash(password, 10);
+    } catch (error) {
+      res.status(500).json({
+        success: false,
+        message: "Error in hashing Password",
+      });
+    }
+
+    // create entry for User
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+      role,
+    });
+
+    res.status(200).json({
+      success: true,
+      message: "User Created Successfully",
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      success: false,
+      message: "User cannot be registered, please try again later",
+    });
+  }
+};
+
+exports.login = async (req, res) => {
+  try {
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      success: false,
+      message: "User cannot be logined, please try again later",
+    });
+  }
+};
